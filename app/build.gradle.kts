820f6f0ef07e76831f10d8220c14a934b78f86e5
Kabir Nasir <iamkabirnasir@gmail.com>
2026-05-04 19:01:13 +0530

    working changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,8 @@ android {
 
 dependencies {
     // MediaPipe LLM Inference API
-    implementation("com.google.mediapipe:tasks-genai:0.10.20")
+    implementation("com.google.mediapipe:tasks-genai:0.10.35")
+    implementation("com.google.mediapipe:tasks-vision:0.10.35")
 
     // CameraX core library using the camera2 implementation
     val cameraxVersion = "1.3.3"
